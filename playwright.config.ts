4c5bb3a4d3a0ddc7154d34e5e4e590b6ae1e1a8a
Gustavo <nilusweb@gmail.com>
2026-09-25 20:21:36 -0300

    nuevos cambios

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,19 +24,19 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['html', { open: 'always' }]
+    ['html', { open: process.env.CI ? 'never' : 'always' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
      /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on-first-retry',
-    trace: 'on',
+    trace: 'off',
     //video: 'retain-on-failure',
-    video: 'on',
+    video: 'off',
   },
 
   /* Configure projects for major browsers */
